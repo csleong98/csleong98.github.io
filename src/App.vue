ea11056f806f525219b5a1c318e514f774495863
csleong98 <macintoshleong@gmail.com>
2019-10-29 21:54:02 +0800

    front end updates

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,39 @@
 <template>
   <div id="app">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <router-link class="navbar-item" to="/">
-          <p>Chee Seng Leong</p>
-        </router-link>
+    <div class="container">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <router-link class="navbar-item" to="/">
+            <p>Chee Seng Leong</p>
+          </router-link>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
-          data-target="navbarBasicExample" :class="{ 'is-active': showNav }" @click="showNav = !showNav">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showNav }">
-        <div class="navbar-start">
-
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+            data-target="navbarBasicExample" :class="{ 'is-active': showNav }" @click="showNav = !showNav">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
 
-        <div class="navbar-end">
-          <router-link to="/" class="navbar-item">
-            UX Designs
-          </router-link>
-          <router-link to="/illustrations" class="navbar-item">
-            Illustrations
-          </router-link>
-          <router-link to="/about" class="navbar-item">
-            About
-          </router-link>
+        <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showNav }">
+          <div class="navbar-end">
+            <router-link to="/" class="navbar-item">
+              UX Projects
+            </router-link>
+            <router-link to="/about" class="navbar-item">
+              About
+            </router-link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
 
 
     <div class="container">
-        <router-view />
+      <router-view />
     </div>
-  
+
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
@@ -50,10 +45,9 @@
 </template>
 
 <script>
-
   export default {
     name: 'App',
-    data: function() {
+    data: function () {
       return {
         showNav: false
       }
@@ -61,13 +55,70 @@
   }
 </script>
 
-<style>
+<style lang="scss">
+  @import "~@/styles/style.sass";
+
   #app {
     min-height: 100vh;
     overflow: hidden;
     display: block;
     position: relative;
-    padding-bottom: 168px; /* height of the footer */
+    padding-bottom: 168px;
+    /* height of the footer */
+  }
+
+  .navbar {
+    margin: 1rem 0 1rem 0;
+    router-link {
+      font-size: 1.5rem;
+    }
+  }
+
+  .header {
+    display: grid;
+    grid-template-columns: 3fr minmax(250px, 350px);
+    grid-template-rows: auto;
+
+    @media screen and (max-width: $large-phones) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+      grid-template-areas:
+        " image "
+        " text1 "
+        " text2 "
+        " text3 ";
+    }
+
+    grid-gap: 1rem;
+    margin: 4rem auto;
+    width: 90%;
+    grid-template-areas: 
+    " text1 image "
+    " text2 image "
+    " text3 image ";
+  }
+
+  .header-text-1 {
+    background-color: bisque;
+    font-size: 3rem;
+    grid-area: text1;
+  }
+
+  .header-text-2 {
+    background-color: aquamarine;
+    font-size: 1.5rem;
+    grid-area: text2;
+  }
+
+  .header-text-3 {
+    background-color: aquamarine;
+    font-size: 1.5rem;
+    grid-area: text3;
+  }
+
+  .header-image {
+    background-color: chocolate;
+    grid-area: image;
   }
 
   footer {
@@ -75,26 +126,8 @@
     bottom: 0;
     width: 100%;
   }
-
-  .header {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-row-gap: 1rem;
-    margin: 4rem auto;
-    width: 90%;
-  }
-
-  .header-text {
-    font-size: 3rem;
-  }
-
-  .header-secondary-text {
-    font-size: 1.5rem;
-  }
-
 </style>
 
 <style lang="sass">
   @import "~bulma/bulma.sass";
-  @import "~@/styles/style.sass";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="home">
-    <div class="header">
-      <p class="header-text">Hi there. I'm a User Experience Designer</p>
-      <p class="header-secondary-text">Aspire to improve the world with good design</p>
-      <p class="header-secondary-text">Previously designed in <a href="https://futurelab.my" target="_blank">FutureLab</a></p>
-    </div>
+    <div class="container">
+      <div class="header">
+        <p class="header-text-1">Hi there. I'm a User Experience Designer</p>
+        <p class="header-text-2">Aspire to improve the world with good design</p>
+        <p class="header-text-3">Previously designed in <a href="https://futurelab.my" target="_blank">FutureLab</a></p>
+        <p class="header-image">My Astronaut Image</p>
+      </div>
+      <div>
+        <h3 class="subheading">My Recent Work</h3>
+      </div>
+      <div class="grid-container" v-for="(section, index) in Object.keys(entries)" :key="index">
+        <div class="card" v-for="entry in entries[section]" :key="entry.id">
+          <div class="card-top">
+            <img :src="entry.thumbnail" alt="">
+          </div>
 
-    <div class="grid-container" v-for="(section, index) in Object.keys(entries)" :key="index">
-      <div class="card" v-for="entry in entries[section]" :key="entry.id">
-        <div class="card-top">
-          <img :src="entry.thumbnail" alt="">
-        </div>
-
-        <div class="card-content">
-          <div class="content">
-            <h3>{{entry.title}}</h3>
-            <p class="description">{{entry.description}}</p>
-            <time class="date">{{entry.date}}</time>
-            <hr>
-            <a class="" @click="$router.push({name: entry.id})">View Case Study</a>
+          <div class="card-content">
+            <div class="content">
+              <h4>{{entry.title}}</h4>
+              <p class="description">{{entry.description}}</p>
+              <a class="" @click="$router.push({name: entry.id})">View Case Study</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -40,13 +44,22 @@
 </script>
 
 <style lang="scss" scoped>
-@import "~@/styles/style.sass";
+  @import "~@/styles/style.sass";
+
+  .subheading {
+    text-transform: uppercase;
+    margin: auto;
+    width: 90%;
+  }
+
   .grid-container {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-    @media screen and (max-width: $large-phones){
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+
+    @media screen and (max-width: $large-phones) {
       grid-template-columns: repeat(1, 1fr);
     }
+
     grid-gap: 1rem;
     margin: 2rem auto;
     width: 90%;
@@ -73,11 +86,10 @@
   .card-content {
     padding: 1.5rem;
     text-align: left;
-    height: 100%;
   }
 
-  .card-content > a {
-      padding: 1rem 2rem 2rem;
-      align-items: flex-end;
+  .card-content>a {
+    padding: 1rem 2rem 2rem;
+    align-items: flex-end;
   }
 </style>
