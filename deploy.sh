d13ab0ca575fef3b28913f46d87cbdf7e8ce5a7c
csleong98 <macintoshleong@gmail.com>
2019-10-31 15:19:56 +0800

    updated deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+echo 'www.csleong.me' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:csleong98/portfolio.git master:gh-pages
+
+cd -
