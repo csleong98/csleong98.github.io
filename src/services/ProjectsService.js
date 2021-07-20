5d54dd0616c90ce8d6ca2ce9464276022d97fc80
csleong98 <macintoshleong@gmail.com>
2021-07-20 21:57:26 +0800

    removed security keys

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -4,7 +4,7 @@ const Axios = axios.create({
     baseURL: "https://api.airtable.com/v0/appeeinoM5v6Dynze/Projects"
 });
 
-const airtableAPIKey = 'keyBwAx7Ayqe03zUm'
+const airtableAPIKey = ''
 
 Axios.defaults.headers.common = {'Authorization': `Bearer ` + airtableAPIKey}
 
